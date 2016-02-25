d0414f2df6b45a0730e4f7bce1b4ea5b23f36d6b
Maxim Ryndin <rgmasim@gmail.com>
2016-02-25 04:31:06 +0300

    Correct test.

--- test/flusher.sh
+++ test/flusher.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-time node src/index.js --getErrors | bunyan
+time node src/index.js --getErrors | ./node_modules/.bin/bunyan
--- test/worker.sh
+++ test/worker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-time node src/index.js | bunyan
+time node src/index.js --messageInterval 1 | ./node_modules/.bin/bunyan
