27afb50dab823445f8a6545bc832241962553950
Maxim Ryndin <rgmasim@gmail.com>
2016-02-25 04:33:49 +0300

    Add info printout.

--- src/lib/writer.js
+++ src/lib/writer.js
@@ -5,14 +5,20 @@ module.exports = function Writer(state, logger, callback) {
     this.run = function () {
 
         state.timers.intervals.lock = setInterval(function () {
+            var message = getMessage();
             state.client
                 .multi()
                 .set(state.storage.lock, state.id, 'XX', 'PX', state.options.lockTTL, function (error) {
                     if (error) callback(error);
                 })
-                .rpush('messages', getMessage(), function (error, value) {
+                .rpush('messages', message, function (error, value) {
                     if (error) callback(error);
-                    if (!error) logger.debug('pushed message, size', value);
+                    if (!error) {
+                        logger.debug('pushed message, size', value);
+                        if (!(message % 5000)) {
+                            logger.info('pushed message', message);
+                        }
+                    }
                 })
                 .exec();
         }, state.options.messageInterval);
